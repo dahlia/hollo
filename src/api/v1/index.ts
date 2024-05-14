555aef7afa69fb31b3f9457f52fecd66f9c0e577
Hong Minhee <hong@minhee.org>
2024-05-14 16:08:57 +0900

    GET /api/v1/preferences

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { tokenRequired } from "../../oauth";
 import accounts from "./accounts";
 import apps from "./apps";
 
@@ -6,5 +7,16 @@ const app = new Hono();
 
 app.route("/apps", apps);
 app.route("/accounts", accounts);
+
+app.get("/preferences", tokenRequired, (c) => {
+  return c.json({
+    // TODO
+    "posting:default:visibility": "public",
+    "posting:default:sensitive": false,
+    "posting:default:language": null,
+    "reading:expand:media": "default",
+    "reading:expand:spoilers": false,
+  });
+});
 
 export default app;
