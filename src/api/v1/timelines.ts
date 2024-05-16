b3c25ba69d72b13e376d2140fffe4dc88b55decb
Hong Minhee <hong@minhee.org>
2024-05-16 10:51:21 +0900

    GET /api/v1/timelines/public

--- src/api/v1/timelines.ts
+++ src/api/v1/timelines.ts
@@ -9,22 +9,49 @@ import { posts } from "../../schema";
 
 const app = new Hono<{ Variables: Variables }>();
 
+const timelineQuerySchema = z.object({
+  max_id: z.string().uuid().optional(),
+  since_id: z.string().uuid().optional(),
+  min_id: z.string().uuid().optional(),
+  limit: z
+    .string()
+    .transform((v) => Number.parseInt(v))
+    .optional(),
+});
+
+app.get("/public", zValidator("query", timelineQuerySchema), async (c) => {
+  const query = c.req.valid("query");
+  const timeline = await db.query.posts.findMany({
+    where: and(
+      eq(posts.visibility, "public"),
+      query.max_id == null ? undefined : lte(posts.id, query.max_id),
+      query.min_id == null ? undefined : gte(posts.id, query.min_id),
+    ),
+    with: {
+      account: true,
+      application: true,
+      replyTarget: true,
+      sharing: {
+        with: {
+          account: true,
+          application: true,
+          replyTarget: true,
+          mentions: { with: { account: { with: { owner: true } } } },
+        },
+      },
+      mentions: { with: { account: { with: { owner: true } } } },
+    },
+    orderBy: [desc(posts.id)],
+    limit: query.limit ?? 20,
+  });
+  return c.json(timeline.map(serializePost));
+});
+
 app.get(
   "/home",
   tokenRequired,
   scopeRequired(["read:statuses"]),
-  zValidator(
-    "query",
-    z.object({
-      max_id: z.string().uuid().optional(),
-      since_id: z.string().uuid().optional(),
-      min_id: z.string().uuid().optional(),
-      limit: z
-        .string()
-        .transform((v) => Number.parseInt(v))
-        .optional(),
-    }),
-  ),
+  zValidator("query", timelineQuerySchema),
   async (c) => {
     const owner = c.get("token").accountOwner;
     if (owner == null) {
