8b0abc6f57772f63161fd751d6b4177477a0c371
Hong Minhee <hong@minhee.org>
2024-05-25 21:36:57 +0900

    POST /api/v1/tags/:id/unfollow

--- src/api/v1/tags.ts
+++ src/api/v1/tags.ts
@@ -27,4 +27,16 @@ app.post("/:id/follow", scopeRequired(["write:follows"]), async (c) => {
   return c.json({ ...serializeTag(tag, null, c.req.url), following: true });
 });
 
+app.post("/:id/unfollow", scopeRequired(["write:follows"]), async (c) => {
+  const owner = c.get("token").accountOwner;
+  if (owner == null) {
+    return c.json({ error: "This method requires an authenticated user" }, 422);
+  }
+  const tag = c.req.param("id");
+  await db.update(accountOwners).set({
+    followedTags: sql`array_remove(${accountOwners.followedTags}, ${tag})`,
+  });
+  return c.json({ ...serializeTag(tag, null, c.req.url), following: false });
+});
+
 export default app;
