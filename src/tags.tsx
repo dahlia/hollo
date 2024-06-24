e9c2570cb81537ef49106600e836a01ccbb7fc14
Hong Minhee <hong@minhee.org>
2024-06-24 14:40:15 +0900

    GET /tags/:tag

---
+++ src/tags.tsx
@@ -0,0 +1,58 @@
+import { and, desc, eq, sql } from "drizzle-orm";
+import { Hono } from "hono";
+import type { FC } from "hono/jsx";
+import { Layout } from "./components/Layout";
+import { Post as PostView } from "./components/Post";
+import { db } from "./db";
+import { type Account, type Medium, type Post, posts } from "./schema";
+
+const app = new Hono();
+
+app.get("/:tag", async (c) => {
+  const tag = c.req.param("tag");
+  const hashtag = `#${tag.toLowerCase()}`;
+  const postList = await db.query.posts.findMany({
+    where: and(sql`${posts.tags} ? ${hashtag}`, eq(posts.visibility, "public")),
+    orderBy: desc(posts.id),
+    with: {
+      account: true,
+      media: true,
+      sharing: {
+        with: {
+          account: true,
+          media: true,
+          replyTarget: { with: { account: true } },
+        },
+      },
+      replyTarget: { with: { account: true } },
+    },
+  });
+  return c.html(<TagPage tag={tag} posts={postList} />);
+});
+
+export interface TagPageProps {
+  tag: string;
+  posts: (Post & {
+    account: Account;
+    media: Medium[];
+    sharing:
+      | (Post & {
+          account: Account;
+          media: Medium[];
+          replyTarget: (Post & { account: Account }) | null;
+        })
+      | null;
+    replyTarget: (Post & { account: Account }) | null;
+  })[];
+}
+
+export const TagPage: FC<TagPageProps> = ({ tag, posts }) => (
+  <Layout title={`#${tag}`}>
+    <h1>#{tag}</h1>
+    {posts.map((post) => (
+      <PostView post={post} />
+    ))}
+  </Layout>
+);
+
+export default app;
