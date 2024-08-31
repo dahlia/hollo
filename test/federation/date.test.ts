33d61ef35075c4b61582dab6c9b84e9c95209f96
Hyunjoon Kim <rickykhj@korea.ac.kr>
2024-08-31 12:49:48 +0900

    Added tests of federation/date.ts

---
+++ test/federation/date.test.ts
@@ -0,0 +1,27 @@
+import { expect, test } from "bun:test";
+import { Temporal } from "@js-temporal/polyfill";
+import { toTemporalInstant, toDate } from "../../src/federation/date";
+
+const date = new Date("2024-08-31T10:00:00");
+const temporalInstant = Temporal.Instant.from(date.toISOString());
+
+test("toTemporalInstant call with null", ()=>{
+  expect(toTemporalInstant(null)).toBe(null)
+});
+
+test("toTemporalInstant call with Date", ()=>{
+  expect(toTemporalInstant(date)).not.toBe(null);
+});
+
+test("toDate call with null", ()=>{
+  expect(toDate(null)).toBe(null);
+});
+
+test("toDate call with Temporal.Instant", ()=>{
+  expect(toDate(temporalInstant)).not.toBe(null);
+});
+
+test("idempotency test", ()=>{
+  expect(toDate(toTemporalInstant(date))).not.toBe(null);
+  expect(toTemporalInstant(toDate(temporalInstant))).not.toBe(null);
+});
